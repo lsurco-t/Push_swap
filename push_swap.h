bdfb6e10a63091f5aafdc2cf5c9511fac83cf147
Lucio <lucio.surcotaype@outlook.com>
2025-06-10 10:02:31 +0300

    added first files

---
+++ push_swap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/10 09:54:14 by lsurco-t          #+#    #+#             */
+/*   Updated: 2025/06/10 09:58:22 by lsurco-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stnctl.h>
+#include "libft/libft.h"
+
+#endif
