0a940e32c855f0a17079ec9cc3a458f6493135ec
Lucio <lucio.surcotaype@outlook.com>
2025-06-11 10:59:30 +0300

    new changes

--- push_rotate.c
+++ push_rotate.c
@@ -6,7 +6,7 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 10:53:25 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/06/11 10:55:25 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/06/11 10:58:56 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,20 +30,31 @@ void pb(int *stack_a, int *stack_b, int *size_a, int *size_b)
 }
 void ra(int *stack_a, int size_a)
 {
+	int i;
+
+	i = 0;
 	if (size_a <= 1)
 		return;
 	int temp = stack_a[0];
-	for (int i = 0; i < size_a - 1; i++)
+	while (i < size_a - 1)
+	{
 		stack_a[i] = stack_a[i + 1];
+		i++;
+	}
 	stack_a[size_a - 1] = temp;
 }
 void rb(int *stack_b, int size_b)
 {
+	int i;
+	i = 0;
 	if (size_b <= 1)
 		return;
 	int temp = stack_b[0];
-	for (int i = 0; i < size_b - 1; i++)
+	while (1 < size_b - 1)
+	{
 		stack_b[i] = stack_b[i + 1];
+		i++;
+	}
 	stack_b[size_b - 1] = temp;
 }
 void rr(int *stack_a, int *stack_b, int size_a, int size_b)
