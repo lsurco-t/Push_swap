fdbbd4661b7883612e075c22e077b403f5f13962
Lucio <lucio.surcotaype@outlook.com>
2025-06-16 13:24:32 +0300

    modified push function

--- sources/push.c
+++ sources/push.c
@@ -6,7 +6,7 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 10:53:25 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/06/13 12:10:03 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/06/16 13:24:11 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,46 @@
 
 void	pa(int *stack_a, int *stack_b, int *size_a, int *size_b)
 {
+	int i;
+	
 	if (*size_b == 0)
 		return ;
-	stack_a[*size_a] = stack_b[*size_b - 1];
+	i = *size_a;
+	while (i > 0)
+ 	{
+  		stack_a[i] = stack_a[i - 1];
+  		i--;
+	}
+	stack_a[0] = stack_b[0];
 	(*size_a)++;
+	i = *size_b - 1;
+ 	while (i > 0)
+ 	{
+  		stack_b[i] = stack_b[i - 1];
+ 		i--;
+	}
 	(*size_b)--;
 }
 
 void	pb(int *stack_a, int *stack_b, int *size_a, int *size_b)
 {
+	int i;
+	
 	if (*size_a == 0)
 		return ;
-	stack_b[*size_b] = stack_a[*size_a - 1];
+	i = *size_b;
+	while (i > 0)
+ 	{
+  		stack_b[i] = stack_b[i - 1];
+  		i--;
+	}
+	stack_b[0] = stack_a[0];
 	(*size_b)++;
+	i = *size_a - 1;
+ 	while (i > 0)
+ 	{
+  		stack_a[i] = stack_a[i - 1];
+ 		i--;
+	}
 	(*size_a)--;
 }
