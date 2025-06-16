d399c23befaa8b4501e72c006731ab86833c5856
Lucio <lucio.surcotaype@outlook.com>
2025-06-16 13:07:50 +0300

    changes to push_swap.c

--- sources/push_swap.c
+++ sources/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 11:08:36 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/06/16 13:00:51 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/06/16 13:05:38 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ void memory_cleanup(int *stack_a, int *stack_b)
 	if (stack_b)
 		free(stack_b);
 }
-void memory_allocation(int **stack_a, int **stack_b, int size_a, int size_b)
+void memory_allocation(int **stack_a, int **stack_b, int size_a)
 {
 	*stack_a = malloc(size_a * sizeof(int));
 	if (!*stack_a)
 		return;
-	*stack_b = malloc(size_b * sizeof(int));
+	*stack_b = malloc(size_a* sizeof(int));
 	if (!*stack_b)
 	{
 		free(*stack_a);
@@ -43,7 +43,7 @@ int	main(int ac, char **av)
 		ft_error();
 	size_a = ac - 1;
 	size_b = 0;
-	memory_allocation(&stack_a, &stack_b, size_a, size_b);
+	memory_allocation(&stack_a, &stack_b, size_a);
 	if (!stack_a || !stack_b)
 	{
 		memory_cleanup(stack_a, stack_b);
