5df8dc727cb0d53a09ceb845c3a8837c7c36880e
Lucio <lucio.surcotaype@outlook.com>
2025-06-18 13:16:11 +0300

    created file to validate input

---
+++ sources/validation.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validation.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 13:15:42 by lsurco-t          #+#    #+#             */
+/*   Updated: 2025/06/18 13:16:03 by lsurco-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	is_valid_int(const char *str)
+{
+	long	num;
+	int		i;
+	int		sign;
+
+	i = 0;
+	sign = 1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	if (str[i] == '\0')
+		return (0);
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	num = ft_atoi(str);
+	if (num > 2147483647 || num < -2147483648)
+		return (0);
+	return (1);
+}
+
+void	validate_input(int ac, char **av)
+{
+	int	i;
+
+	if (ac < 2)
+		ft_error();
+	i = 1;
+	while (i < ac)
+	{
+		if (!is_valid_int(av[i]))
+			ft_error();
+		i++;
+	}
+}
