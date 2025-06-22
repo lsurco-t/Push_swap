6ebc5b4c2d6f5158946e7a4ab1eea521f74fef69
Lucio <lucio.surcotaype@outlook.com>
2025-06-22 18:59:24 +0300

    pointer assignment to chunk function

--- sources/sorting.c
+++ sources/sorting.c
@@ -6,13 +6,13 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 18:52:32 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/06/22 18:48:53 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/06/22 18:58:09 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void	small_chunk(int *stack_a, int *stack_b, int size_a, int size_b)
+void	small_chunk(int *stack_a, int *stack_b, int *size_a, int *size_b)
 {
 	int	chunk_size;
 	int	*sorted;
@@ -21,23 +21,23 @@ void	small_chunk(int *stack_a, int *stack_b, int size_a, int size_b)
 	int	max;
 	int	pos;
 
-	sorted = copy_and_sort(stack_a, size_a);
-	chunk_size = size_a / 10;
+	sorted = copy_and_sort(stack_a, *size_a);
+	chunk_size = *size_a / 10;
 	i = 0;
 	while (i < 10)
 	{
 		min = sorted[i * chunk_size];
 		if (i == 9)
-			max = sorted[size_a - 1];
+			max = sorted[*size_a - 1];
 		else
 			max = sorted[(i + 1) * chunk_size - 1];
-		while (has_chunk_member(stack_a, size_a, min, max))
+		while (has_chunk_member(stack_a, *size_a, min, max))
 		{
-			pos = find_closest_chunk_member(stack_a, size_a, min, max);
+			pos = find_closest_chunk_member(stack_a, *size_a, min, max);
 			if (pos != -1)
 			{
-				rotate_stack_top(stack_a, size_a, pos, 'a');
-				pb(stack_a, stack_b, &size_a, &size_b);
+				rotate_stack_top(stack_a, *size_a, pos, 'a');
+				pb(stack_a, stack_b, *size_a, *size_b);
 			}
 		}
 		i++;
@@ -45,7 +45,7 @@ void	small_chunk(int *stack_a, int *stack_b, int size_a, int size_b)
 	free(sorted);
 }
 
-void	large_chunk(int *stack_a, int *stack_b, int size_a, int size_b)
+void	large_chunk(int *stack_a, int *stack_b, int *size_a, int *size_b)
 {
 	int	chunk_size;
 	int	*sorted;
@@ -54,23 +54,23 @@ void	large_chunk(int *stack_a, int *stack_b, int size_a, int size_b)
 	int	max;
 	int	pos;
 
-	sorted = copy_and_sort(stack_a, size_a);
-	chunk_size = size_a / 10;
+	sorted = copy_and_sort(stack_a, *size_a);
+	chunk_size = *size_a / 10;
 	i = 0;
 	while (i < 10)
 	{
 		min = sorted[i * chunk_size];
 		if (i == 9)
-			max = sorted[size_a - 1];
+			max = sorted[*size_a - 1];
 		else
 			max = sorted[(i + 1) * chunk_size - 1];
-		while (has_chunk_member(stack_a, size_a, min, max))
+		while (has_chunk_member(stack_a, *size_a, min, max))
 		{
-			pos = find_closest_chunk_member(stack_a, size_a, min, max);
+			pos = find_closest_chunk_member(stack_a, *size_a, min, max);
 			if (pos != -1)
 			{
-				rotate_stack_top(stack_a, size_a, pos, 'a');
-				pb(stack_a, stack_b, &size_a, &size_b);
+				rotate_stack_top(stack_a, *size_a, pos, 'a');
+				pb(stack_a, stack_b, *size_a, *size_b);
 			}
 		}
 		i++;
