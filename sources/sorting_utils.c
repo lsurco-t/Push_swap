d63b9ddc54041daccd46a202ad4f146b5548d844
Lucio <lucio.surcotaype@outlook.com>
2025-06-23 13:45:56 +0300

    saving functions

--- sources/sorting_utils.c
+++ sources/sorting_utils.c
@@ -6,7 +6,7 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 23:01:53 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/06/23 11:49:15 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/06/23 13:41:40 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,4 +52,52 @@ int	find_min_pos(int *stack, int size)
 		i++;
 	}
 	return (min_pos);
+}
+
+void	rotate_stack_top(int *stack, int size, int cost, char stack_id)
+{
+	if (cost > 0)
+	{
+		while (cost > 0)
+		{
+			if (stack_id == 'a')
+				ra(stack, size);
+			else
+				rb(stack, size);
+			cost--;
+		}
+	}
+	else if (cost < 0)
+	{
+		while (cost < 0)
+		{
+			if (stack_id == 'a')
+				rra(stack, size);
+			else
+				rrb(stack, size);
+			cost++;
+		}
+	}
+}
+
+int	cost_sort(int size, int pos)
+{
+	if (pos <= size / 2)
+		return (pos);
+	else
+		return (pos - size);
+}
+
+int	is_stack_sorted(int *stack, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		if (stack[i] > stack[i + 1])
+			return (0);
+		i++;
+	}
+	return (1);
 }
--- sources/sorting_utils_two.c
+++ sources/sorting_utils_two.c
@@ -6,56 +6,10 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 12:36:26 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/06/23 12:46:20 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/06/23 13:41:20 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void	rotate_stack_top(int *stack, int size, int cost, char stack_id)
-{
-	if (cost > 0)
-	{
-		while (cost > 0)
-		{
-			if (stack_id == 'a')
-				ra(stack, size);
-			else
-				rb(stack, size);
-			cost--;
-		}
-	}
-	else if (cost < 0)
-	{
-		while (cost < 0)
-		{
-			if (stack_id == 'a')
-				rra(stack, size);
-			else
-				rrb(stack, size);
-			cost++;
-		}
-	}
-}
 
-int	cost_sort(int size, int pos)
-{
-	if (pos <= size / 2)
-		return (pos);
-	else
-		return (pos - size);
-}
-
-int	is_stack_sorted(int *stack, int size)
-{
-	int	i;
-
-	i = 0;
-	while (i < size - 1)
-	{
-		if (stack[i] > stack[i + 1])
-			return (0);
-		i++;
-	}
-	return (1);
-}
