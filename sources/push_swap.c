8ed3bf13853430f3b1011bc6bafc0a7bf7e60a2b
Lucio <lucio.surcotaype@outlook.com>
2025-06-28 13:03:10 +0300

    check for no input

--- sources/push_swap.c
+++ sources/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 11:08:36 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/06/26 16:40:12 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/06/28 13:00:35 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ int	main(int ac, char **av)
 	int	*stack_b;
 	int	size_a;
 	int	size_b;
-
+	
+	if (ac < 2)
+		return (0);
 	stack_a = validate_input(ac, av, &size_a);
 	size_b = 0;
 	stack_b = malloc(size_a * sizeof(int));
